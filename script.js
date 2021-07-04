d121f029fb4053205995ec9de1ded7be13e56656
Kevin Howard <kchoward48@gmail.com>
2021-07-04 15:12:14 -0400

    Finalized initial viewport.

--- script.js
+++ script.js
@@ -1,12 +1,2 @@
-$('#resume').click(function(){
-$(document).scrollTop(100) // any value you need
-});
 
-$('#sample').click(function(){
-$(document).scrollTop(100) // any value you need
-});
-
-$('#contact').click(function(){
-$(document).scrollTop(100) // any value you need
-});
 
