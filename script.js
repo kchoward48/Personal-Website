f7f5cc162275841baa8a9eece33cfa510601e5eb
Kevin Howard <kchoward48@gmail.com>
2021-07-04 12:02:27 -0400

    Basic layout for landing display.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+$('#resume').click(function(){
+$(document).scrollTop(100) // any value you need
+});
+
+$('#sample').click(function(){
+$(document).scrollTop(100) // any value you need
+});
+
+$('#contact').click(function(){
+$(document).scrollTop(100) // any value you need
+});
 
